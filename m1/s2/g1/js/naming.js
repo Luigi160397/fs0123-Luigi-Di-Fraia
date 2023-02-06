c6c262f5cc1728448fdea2e48bcc5ef8d1b5e0a8
Luigi160397 <luigi.difraia.97@gmail.com>
2023-02-06 16:35:02 +0100

    js1

---
+++ m1/s2/g1/js/naming.js
@@ -0,0 +1,17 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = '<p>Sei un Ghiro!</p>';
+console.log(petPreferito);
+var PetPreferito = '<p>Sei un Ippopotamo!</p>';
+console.log(PetPreferito);
+var $pet = '<p>Sei uno Struzzo!</p>';
+console.log($pet);
+var _pet = '<p>Sei un Leone!</p>';
+console.log(_pet);
+var _pet2 = '<p>Sei una Pantera!</p>';
+console.log(_pet2);
+
+//var 12 = 'numero';
